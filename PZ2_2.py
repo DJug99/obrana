802fbac653d3ff4a49b345da172a146c19f6736c
Daniel Jug <daniel.jug@tvz.hr>
2023-04-11 12:01:49 +0200

    popravak koda

--- PZ2_2.py
+++ PZ2_2.py
@@ -1,4 +1,5 @@
 from datetime import date
+
 korisnici = []
 kategorije = []
 prodaje = []
@@ -33,6 +34,7 @@ for i in range(1,br_kategorija+1):
     kategorija = {}
     kategorija['naziv'] = input(f'Unesite naziv {i}. kategorije: ')
     kategorija['artikli'] = []
+
     br_artikala = int(input(f'Unesite broj artikala za {i}. kategoriju: '))
     for j in range(1,br_artikala+1):
         artikl = {}
@@ -41,6 +43,7 @@ for i in range(1,br_kategorija+1):
         artikl['cijena'] = float(input(f'Unesite cijenu {j}. artikla: '))
         kategorija['artikli'].append(artikl)
     kategorije.append(kategorija)
+
 br_prodaja = int(input('Unesite broj prodaja: '))
 for i in range(1,br_prodaja+1):
     prodaja = {}
@@ -48,21 +51,28 @@ for i in range(1,br_prodaja+1):
     mjesec = int(input(f'Unesite mjesec isteka {i}. prodaje: '))
     godina = int(input(f'Unesite godinu isteka {i}. prodaje: '))
     prodaja['datum'] = date(godina ,mjesec, dan)
+
     print(f'Odaberite korisnika {i}. prodaje')
     for j,korisnik in enumerate(korisnici, start = 1):
         print(f"\t{j}. {korisnik['ime']} {korisnik['prezime']}")
+
     odabrani_korisnik = int(input('Odabrani korisnik: '))
     prodaja['korisnik'] = korisnici[odabrani_korisnik-1]
+
     print(f'Odaberite kategoriju {i}. prodaje')
     for j,kategorija in enumerate(kategorije, start = 1):
             print(f"{j}. {kategorija['naziv']}")
+
     odabrana_kategorija =int(input('Odabrana kategorija: '))
+
     print(f'Odaberite artikl {i}. prodaje')
     for j,artikl in enumerate(kategorije[odabrana_kategorija-1]['artikli'], start=1):
             print(f"{j}. {kategorije[odabrana_kategorija-1]['artikli'][j-1]['naslov']}")
+
     odabrani_artikl = int(input('Odabrani artikl: '))
     prodaja['artikl'] = kategorije[odabrana_kategorija-1]['artikli'][odabrani_artikl-1]
     prodaje.append(prodaja)
+
 for i,prodaja in enumerate(prodaje, start = 1):
     print(f"Prodaja {i}")
     print('Informacije o korisniku: ')
